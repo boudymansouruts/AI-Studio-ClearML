b9b17297dbd2c6786cedb975ed2c63a8306625a2
boudymansouruts <boudy.mansour@student.uts.edu.au>
2025-04-03 19:44:26 +1100

    Update step3_train_model.py

--- step3_train_model.py
+++ step3_train_model.py
@@ -14,7 +14,7 @@ logger = Logger.current_logger()
 
 # Arguments
 args = {
-    'dataset_task_id': 'ac464e608e2b4eaea85228a738c68300',
+    'dataset_task_id': 'e5d87ceb1090414b889b91d8a7e3ea01',
 }
 task.connect(args)
 
